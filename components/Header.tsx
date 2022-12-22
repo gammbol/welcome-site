47f697cf0965c8a6d2ba41894b4bfa97ac67ce98
gammbol <71922180+gammbol@users.noreply.github.com>
2022-12-22 23:22:42 +0300

    started site developing

---
+++ components/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { SocialIcon } from 'react-social-icons';
+
+type Props = {}
+
+export default function ({}: Props) {
+  return (
+    <header>
+      <div>
+        {/* Social Icons */}
+      </div> 
+    </header>
+  );
+}
